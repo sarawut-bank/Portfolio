2d5bd14803e23537a82459a9109f3091e1b117c3
Bank <85855675+sarawut-bank@users.noreply.github.com>
2023-09-06 16:57:49 +0700

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,45 @@
+function work1(){
+    console.log("work1")
+    var modal1 = document.getElementById("modal1");
+    modal1.style.display = "block";
+}
+
+function work2(){
+    console.log("work2")
+    var modal1 = document.getElementById("modal2");
+    modal1.style.display = "block";
+}
+
+function work3(){
+    console.log("work3")
+    var modal1 = document.getElementById("modal3");
+    modal1.style.display = "block";
+}
+
+function work4(){
+    console.log("work4")
+    var modal1 = document.getElementById("modal4");
+    modal1.style.display = "block";
+}
+
+function work5(){
+    console.log("work5")
+    var modal1 = document.getElementById("modal5");
+    modal1.style.display = "block";
+}
+
+function closeModal() {
+    console.log("close")
+    var modal = document.getElementById("modal1");
+    var modal2 = document.getElementById("modal2");
+    var modal3 = document.getElementById("modal3");
+    var modal4 = document.getElementById("modal4");
+    var modal5 = document.getElementById("modal5");
+    modal.style.display = "none";
+    modal2.style.display = "none";
+    modal3.style.display = "none";
+    modal4.style.display = "none";
+    modal5.style.display = "none";
+}
+
+
